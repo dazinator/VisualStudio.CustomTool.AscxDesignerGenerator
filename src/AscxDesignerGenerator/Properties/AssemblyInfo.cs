6e1e7a063043c7003aa6884211e0478692d4f394
dazinator <darrell.tunnell@googlemail.com>
2017-03-05 15:15:53 +0000

    Make assembly com visible.

--- src/AscxDesignerGenerator/Properties/AssemblyInfo.cs
+++ src/AscxDesignerGenerator/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("402a6a31-95e7-4716-a456-a7226eab0d62")]
